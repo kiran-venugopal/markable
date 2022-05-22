ca268b32f5d06ecfb6c8823c75040cb7ccb5aefc
Kiran V <kiranvenugopal07@gmail.com>
2022-05-22 17:42:14 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,15 @@ function App() {
     <RecoilRoot>
       <div className="App">
         <Navbar />
+        <iframe
+          width="1280"
+          height="720"
+          src="https://www.youtube.com/embed/ldyqHK0tbSc"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
         <div className="editor-container">
           <Sidebar />
           <EditorSection />
